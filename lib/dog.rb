cd03778ce631826459fc5d03905606a10b375bbd
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-22 19:44:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,11 @@
 class Dog
+  #the first methos takes argument of a dog's name and sets that argument equal to a variable this_dogs_name
+#this method is responsible for "setting" 
   def name=(dog_name)
     this_dogs_name = dog_name
   end
-
+#this method is responsible for reporting or reading the name 
+#this is responsible for "getting" becasue it gets a porperty for us 
   def name
     this_dogs_name
   end
